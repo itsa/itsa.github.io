914c59f798302404ffddf0139fb921f03bbc9b8d
Marco Asbreuk <marco@itsasbreuk.nl>
2015-03-19 01:02:16 +0100

    new buildfile

--- dist/itsabuild.js
+++ dist/itsabuild.js
@@ -17340,10 +17340,13 @@ module.exports = function (window) {
                 (scrollWidth===(width+1)) && (scrollWidth=width);
             }
 
-            vScrollerVisible = (scrollHeight>height),
-            hScrollerVisible = (scrollWidth>width),
+            vScrollerVisible = (scrollHeight>height);
+            hScrollerVisible = (scrollWidth>width);
             vscroller.toggleClass('itsa-visible', vScrollerVisible);
             hscroller.toggleClass('itsa-visible', hScrollerVisible);
+
+            console.info('vScrollerVisible: '+vScrollerVisible+'  (scrollHeight:'+scrollHeight+' | height:'+height+')');
+            console.info('hScrollerVisible: '+hScrollerVisible+'  (scrollWidth:'+scrollWidth+' | width:'+width+')');
 
             host.scrollTop = scrollTop;
             host.scrollLeft = scrollLeft;
